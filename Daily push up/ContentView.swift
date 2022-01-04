8f9cb8f4701dff6648c80aab5b7c56c56ed7ad28
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2022-01-04 18:08:50 +0100

    Initial Commit

---
+++ Daily push up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daily push up
+//
+//  Created by Maxence Gama on 04/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Daily push up/Daily_push_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_push_upApp.swift
+//  Daily push up
+//
+//  Created by Maxence Gama on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Daily_push_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
